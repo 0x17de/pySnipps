ffd95cc0c88d9c4678d7e29f9a8937bf2eda6d78
Manuel Herrmann <git@icetruck.de>
2013-06-23 23:22:25 +0200

    all done, but tags/language

--- dataprovider.py
+++ dataprovider.py
@@ -15,11 +15,39 @@ class DataProvider:
 		if (not self.existsTable("categories")):
 			self.db.execute("CREATE TABLE 'categories' ( 'id' INTEGER PRIMARY KEY, 'name' TEXT )")
 		if (not self.existsTable("snipps")):
-			self.db.execute("CREATE TABLE 'snipps' ( 'id' INTEGER PRIMARY KEY, 'name' TEXT, 'code' TEXT )")
+			self.db.execute("CREATE TABLE 'snipps' ( 'id' INTEGER PRIMARY KEY, 'cat_id' INTEGER, 'name' TEXT, 'code' TEXT )")
 
 	def getCategories(self):
 		ret = []
 		for row in self.db.execute("SELECT id, name FROM categories ORDER BY name ASC"):
+			ret.append([int(row[0]), row[1]])
+		return ret
+
+	def snipGet(self, id):
+		res = self.db.execute("SELECT id, name, code FROM snipps WHERE id = ? LIMIT 1", (id,))
+		row = res.fetchone()
+		if not row is None:
+			return [int(row[0]), row[1], row[2]]
+		return None
+
+	def snipSave(self, catid, id, name, text):
+		if id is None:
+			self.db.execute("INSERT INTO snipps (cat_id, name, code) VALUES (?, ?, ?)", (catid, name, text))
+		else:
+			self.db.execute("UPDATE snipps SET name = ?, code = ? WHERE id = ?", (name, text, id))
+		self.db.commit()
+
+	def snipDel(self, id):
+		self.db.execute("DELETE FROM snipps WHERE id = ?", (id,))
+		self.db.commit()
+
+	def snipsOfCatDel(self, id):
+		self.db.execute("DELETE FROM snipps WHERE cat_id = ?", (id,))
+		self.db.commit()
+
+	def getEntries(self, id):
+		ret = []
+		for row in self.db.execute("SELECT id, name FROM snipps WHERE cat_id = ? ORDER BY name ASC", (id,)):
 			ret.append([int(row[0]), row[1]])
 		return ret
 
@@ -30,6 +58,7 @@ class DataProvider:
 	def catDel(self, id):
 		self.db.execute("DELETE FROM categories WHERE id = ?", (id,))
 		self.db.commit()
+		self.snipsOfCatDel(id)
 
 	def __del__(self):
 		self.db.close()
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python2
 
-from gi.repository import Gtk
+from gi.repository import Gtk, Gdk
 from dataprovider import *
 
 class HelloWorld:
@@ -14,7 +14,8 @@ class HelloWorld:
 		dlg.destroy()
 
 	def snipNew(self, widget, data=None):
-		self.notify("Not yet!")
+		self.selEnt.unselect_all()
+		self.entSelChanged()
 
 	def catNew(self, widget):
 		res = self.dlgCat.run()
@@ -28,6 +29,8 @@ class HelloWorld:
 
 	def catDel(self, widget):
 		(model, iter) = self.selCat.get_selected()
+		if iter is None:
+			return
 		id = model.get_value(iter, 0)
 		text = model.get_value(iter, 1)
 		dlg = Gtk.MessageDialog(self.window, 0, Gtk.MessageType.INFO, Gtk.ButtonsType.YES_NO, "Delete '%s'?" % text)
@@ -36,21 +39,71 @@ class HelloWorld:
 		if res == Gtk.ResponseType.YES:
 			self.db.catDel(id)
 			self.refreshCategories()
-			self.catSelChanged()
+
+	def snipCopy(self, widget=None):
+		buf = self.snipCode.get_buffer()
+		clip = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
+		clip.set_text(buf.get_text(buf.get_start_iter(), buf.get_end_iter(), True), -1)
+
+	def refreshSnipp(self):
+		snip = self.db.snipGet(self.selEntID)
+		if snip is None:
+			self.snipName.set_text("")
+			self.snipCode.get_buffer().set_text("")
+		else:
+			self.snipName.set_text(snip[1])
+			self.snipCode.get_buffer().set_text(snip[2])
+
+	def snipSave(self, widget=None):
+		if not self.selCatID is None:
+			buf = self.snipCode.get_buffer()
+			text = buf.get_text(buf.get_start_iter(), buf.get_end_iter(), True)
+			self.db.snipSave(self.selCatID, self.selEntID, self.snipName.get_text(), text)
+			self.refreshEntries()
+
+	def snipDel(self, widget=None):
+		(model, iter) = self.selEnt.get_selected()
+		if iter is None:
+			return
+		id = model.get_value(iter, 0)
+		text = model.get_value(iter, 1)
+		dlg = Gtk.MessageDialog(self.window, 0, Gtk.MessageType.INFO, Gtk.ButtonsType.YES_NO, "Delete '%s'?" % text)
+		res = dlg.run()
+		dlg.destroy()
+		if res == Gtk.ResponseType.YES:
+			self.db.snipDel(self.selEntID)
+			self.refreshEntries()
+
+	def refreshEntries(self):
+		ent = self.db.getEntries(self.selCatID)
+		self.lsSnipps.clear()
+		if not self.selCatID is None:
+			for e in ent:
+				self.lsSnipps.append(e)
+		self.entSelChanged()
+
+	def entSelChanged(self, widget=None):
+		(model, iter) = self.selEnt.get_selected()
+		if iter is None:
+			self.selEntID = None
+		else:
+			self.selEntID = model.get_value(iter, 0)
+		self.refreshSnipp()
 
 	def refreshCategories(self):
 		cat = self.db.getCategories()
 		self.lsCat.clear()
 		for c in cat:
 			self.lsCat.append(c)
+		self.catSelChanged()
 
 	def catSelChanged(self, widget=None):
 		(model, iter) = self.selCat.get_selected()
 		if iter is None:
 			self.selCatID = None
 		else:
-			id = model.get_value(iter, 0)
-			self.selCatID = id
+			self.selCatID = model.get_value(iter, 0)
+		self.refreshEntries()
 
 	def __init__(self):
 		builder = Gtk.Builder()
@@ -72,14 +125,26 @@ class HelloWorld:
 		actCatDel = builder.get_object("actCatDel")
 		actCatDel.connect("activate", self.catDel)
 
-		self.lsSnippet = builder.get_object("lsSnipps")
+		self.selEnt = builder.get_object("selEnt")
+		self.selEnt.connect("changed", self.entSelChanged)
+		self.snipName = builder.get_object("snipName")
+		self.snipLang = builder.get_object("snipLang")
+		self.snipTags = builder.get_object("snipTags")
+		self.snipCode = builder.get_object("snipCode")
+
+		self.lsSnipps = builder.get_object("lsSnipps")
 		actSnipNew = builder.get_object("actSnipNew")
 		actSnipNew.connect("activate", self.snipNew)
+		actSnipCopy = builder.get_object("actSnipCopy")
+		actSnipCopy.connect("activate", self.snipCopy)
+		actSnipSave = builder.get_object("actSnipSave")
+		actSnipSave.connect("activate", self.snipSave)
+		actSnipDel = builder.get_object("actSnipDelete")
+		actSnipDel.connect("activate", self.snipDel)
 
 		self.refreshCategories()
 		self.catSelChanged()
-
-		
+		self.refreshEntries()
 
 	def main(self):
 		Gtk.main()
