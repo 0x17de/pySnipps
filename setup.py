a90d68084c7d9ce333c7c7b018c92a64f45f813b
Manuel Herrmann <git@icetruck.de>
2013-06-26 19:39:40 +0200

    version 0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='pySnipps',
-      version='0.2',
+      version='0.3',
       description='A snippet tool written in python',
       author='Manuel Herrmann',
       author_email='pysnipps@icetruck.de',
