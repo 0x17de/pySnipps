611b86ab1a07b3b98916e6af35ac9bfe8396ab7e
Manuel Herrmann <git@icetruck.de>
2013-06-26 19:34:35 +0200

    simple setup.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python2
+
+from distutils.core import setup
+
+setup(name='pySnipps',
+      version='0.2',
+      description='A snippet tool written in python',
+      author='Manuel Herrmann',
+      author_email='pysnipps@icetruck.de',
+      url='http://pysnipps.icetruck.de/',
+      packages=[],
+     )
