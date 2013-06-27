8a13411258a532f81b842eebf5e6cc94d9f0becd
Manuel Herrmann <git@icetruck.de>
2013-06-27 21:41:43 +0200

    setup fix for the fix

--- setup.py
+++ setup.py
@@ -12,5 +12,5 @@ setup(name='pySnipps',
       packages=["pySnipps"],
 	  package_dir={'pySnipps': 'src/pySnipps'},
 	  package_data={'pySnipps': ['resources/*']},
-	  install_requires=['pyobject >= 3'],
+	  install_requires=['pygobject >= 3'],
      )
