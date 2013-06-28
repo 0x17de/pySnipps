97bf307aeffc8946cb0982afe181bfed3fb236e8
Manuel Herrmann <git@icetruck.de>
2013-06-28 08:09:53 +0200

    0.5 layout fixes working and verified

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='pySnipps',
-      version='0.4',
+      version='0.5',
       description='A snippet tool written in python',
       author='Manuel Herrmann',
       author_email='pysnipps@icetruck.de',
